2c6c0e543df96f86e3ac513766b357d6969ec957
scharlegill <55111969+scharlegill@users.noreply.github.com>
2020-11-05 14:48:25 +0100

    Zwischenstand SmallNumber implementiert und erste Tests

--- Numerics/Numerics/SmallNumber.cs
+++ Numerics/Numerics/SmallNumber.cs
@@ -8,6 +8,21 @@ namespace SharpFast.Numerics
     {
         private int data;
 
+        public SmallNumber(byte number)
+        {
+            data = number * 1000;
+        }
+
+        public SmallNumber(char number)
+        {
+            data = number * 1000;
+        }
+
+        public SmallNumber(short number)
+        {
+            data = number * 1000;
+        }
+
         public SmallNumber(int number)
         {
             if (number >= 2147483)
@@ -24,6 +39,34 @@ namespace SharpFast.Numerics
                 data = int.MaxValue;
             else
                 data = (int)number * 1000;
+        }
+
+        public SmallNumber(long number)
+        {
+            if (number >= 2147483)
+                data = int.MaxValue;
+            else if (number <= -2147483)
+                data = int.MinValue;
+            else
+                data = (int)number * 1000;
+        }
+
+        public SmallNumber(ulong number)
+        {
+            if (number >= 2147483)
+                data = int.MaxValue;
+            else
+                data = (int)number * 1000;
+        }
+
+        public SmallNumber(float number)
+        {
+            if (number >= 2147483.6465)
+                data = int.MaxValue;
+            else if (number <= -2147483.6475)
+                data = int.MinValue;
+            else
+                data = (int)(number * 1000.0 + 0.5);
         }
 
         public SmallNumber(double number)
@@ -46,9 +89,158 @@ namespace SharpFast.Numerics
                 data = (int)(number * (decimal)1000.0 + (decimal)0.5);
         }
 
+
+        public static implicit operator SmallNumber(byte number)
+        {
+            return new SmallNumber(number);
+        }
+
+        public static implicit operator SmallNumber(char number)
+        {
+            return new SmallNumber(number);
+        }
+
+        public static implicit operator SmallNumber(short number)
+        {
+            return new SmallNumber(number);
+        }
+
         public static implicit operator SmallNumber(uint number)
         {
            return new SmallNumber(number);
+        }
+
+        public static explicit operator SmallNumber(long number)
+        {
+            return new SmallNumber(number);
+        }
+
+        public static explicit operator SmallNumber(ulong number)
+        {
+            return new SmallNumber(number);
+        }
+
+        public static explicit operator SmallNumber(float number)
+        {
+            return new SmallNumber(number);
+        }
+
+        public static explicit operator SmallNumber(double number)
+        {
+            return new SmallNumber(number);
+        }
+
+        public static explicit operator SmallNumber(decimal number)
+        {
+            return new SmallNumber(number);
+        }
+
+        public static SmallNumber operator+ (SmallNumber l, SmallNumber r)
+        {
+            SmallNumber tmp;
+
+            long number = (long)l.data + r.data;
+
+            if (number > int.MaxValue)
+                number = int.MaxValue;
+            else if (number < int.MinValue)
+                number = int.MinValue;
+
+            tmp = new SmallNumber();
+
+            tmp.data = (int)number;
+
+            return tmp;
+        }
+
+        public static SmallNumber operator- (SmallNumber l, SmallNumber r)
+        {
+            long number = (long)l.data - r.data;
+
+            if (number > int.MaxValue)
+                number = int.MaxValue;
+            else if (number < int.MinValue)
+                number = int.MinValue;
+
+            SmallNumber tmp = new SmallNumber();
+
+            tmp.data = (int)number;
+
+            return tmp;
+        }
+
+        public static SmallNumber operator* (SmallNumber l, SmallNumber r)
+        {
+            long number = ((long)l.data * r.data) / 1000;
+
+
+            if (number > int.MaxValue)
+                number = int.MaxValue;
+            else if (number < int.MinValue)
+                number = int.MinValue;
+
+            SmallNumber tmp = new SmallNumber();
+
+            tmp.data = (int)number;
+
+            return tmp;
+        }
+
+        public static SmallNumber operator /(SmallNumber l, SmallNumber r)
+        {
+            long right = r.data / 1000;
+
+            long number = (l.data / right);
+
+            if (number > 21474836465)
+                number = 21474836465;
+            else if (number < -21474836475)
+                number = -21474836475;
+
+            SmallNumber tmp = new SmallNumber();
+
+            tmp.data = (int)number;
+
+            return tmp;
+        }
+
+        public static bool operator ==(SmallNumber l, SmallNumber r)
+        {
+            return l.data == r.data;
+        }
+
+        public static bool operator !=(SmallNumber l, SmallNumber r)
+        {
+            return l.data != r.data;
+        }
+
+        public static bool operator >(SmallNumber l, SmallNumber r)
+        {
+            return l.data > r.data;
+        }
+
+        public static bool operator <(SmallNumber l, SmallNumber r)
+        {
+            return l.data < r.data;
+        }
+
+        public static bool operator <=(SmallNumber l, SmallNumber r)
+        {
+            return l.data <= r.data;
+        }
+
+        public static bool operator >=(SmallNumber l, SmallNumber r)
+        {
+            return l.data >= r.data;
+        }
+
+        public static SmallNumber operator %(SmallNumber l, SmallNumber r)
+        {
+            SmallNumber tmp = new SmallNumber();
+                
+            tmp.data = l.data % r.data;
+
+            return tmp;
         }
 
         public override string ToString()
--- Numerics/Tests/UtilsTests.cs
+++ Numerics/Tests/UtilsTests.cs
@@ -18,13 +18,72 @@ namespace Tests
             fixed (uint* rightStart = right)
             fixed (uint* resultStart = result)
             {
-                Utils.Add(leftStart, leftStart + left.Length, rightStart, rightStart + right.Length, resultStart, resultStart + result.Length);
+                //Utils.Add(leftStart, leftStart + left.Length, rightStart, rightStart + right.Length, resultStart, resultStart + result.Length);
             }
 
             Assert.AreEqual(11, result[0], "First segment mismatch.");
             Assert.AreEqual(0, result[1], "Second segment mismatch.");
             Assert.AreEqual(0, result[2], "Third segment mismatch.");
             Assert.AreEqual(0, result[3], "Fourth segment mismatch.");
+        }
+
+        [TestMethod]
+        public unsafe void SmallNumberTests()
+        {
+            {
+                SmallNumber a = new SmallNumber(6);
+                SmallNumber b = new SmallNumber(5);
+
+                SmallNumber resultAdd = a + b;
+
+                Assert.IsTrue(resultAdd == new SmallNumber(11));
+
+                SmallNumber resultSub = a - b;
+
+                Assert.IsTrue(resultSub == new SmallNumber(1));
+
+                SmallNumber resultDiv = a / b;
+
+                Assert.IsTrue(resultDiv == new SmallNumber(1.2));
+
+                SmallNumber resultMult = a * b;
+
+                Assert.IsTrue(resultMult == new SmallNumber(30));
+
+                Assert.IsFalse(a == b);
+                Assert.IsTrue(a != b);
+
+                Assert.IsFalse(a < b);
+                Assert.IsTrue(a > b);
+            }
+
+            {
+                SmallNumber a = new SmallNumber(21474836465);
+                SmallNumber b = new SmallNumber(5000);
+
+                SmallNumber resultAdd = a + b;
+
+                Assert.IsTrue(resultAdd == new SmallNumber(int.MaxValue));
+
+                SmallNumber resultSub = a - b;
+
+                Assert.IsTrue(resultSub == new SmallNumber(2147483.647 - 5000));
+
+                SmallNumber resultDiv = a / b;
+
+               // Assert.IsTrue(resultDiv == new SmallNumber(2147483.647 / 5000));
+
+                SmallNumber resultMult = a * b;
+
+                Assert.IsTrue(resultMult == new SmallNumber(int.MaxValue));
+
+                Assert.IsFalse(a == b);
+                Assert.IsTrue(a != b);
+
+                Assert.IsFalse(a < b);
+                Assert.IsTrue(a > b);
+            }
+
         }
     }
 }
